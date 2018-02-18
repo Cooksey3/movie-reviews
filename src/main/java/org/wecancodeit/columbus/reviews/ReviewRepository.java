1debe1dca4925d572d2db88d330f317840ad88da
cooksey3 <chris@cookseys.org>
2018-02-18 14:32:50 -0500

    Made site more appealing

--- src/main/java/org/wecancodeit/columbus/reviews/ReviewRepository.java
+++ src/main/java/org/wecancodeit/columbus/reviews/ReviewRepository.java
@@ -12,16 +12,23 @@ public class ReviewRepository {
 	Map<Long, Review> reviews = new HashMap<>();
 
 	public ReviewRepository() {
-		Review firstMovie = new Review(902L, "Identity", "Great thiller. Excellent ending.", "Review Category",
-				"Content", "/images/identity.jpg", "Synopsis", "James Mangold");
-		Review secondMovie = new Review(121L, "John Wick", "Best. Movie. Ever.", "Review Category", "Content",
-				"/images/john-wick.jpg", "Synopsis", "Chad Stahelski, David Leitch");
-		Review thirdMovie = new Review(432L, "Texas Chainsaw Massacre", "Strange ending...needs reworking.",
-				"Review Category", "Content", "/images/texas-chainsaw-massacre.jpg", "Synopsis", "Tobe Hooper");
-		Review fourthMovie = new Review(223L, "Silence of the Lambs", "Movie description", "Review Category", "Content",
-				"/images/silence-of-the-lambs.jpg", "Synopsis", "Jonathan Demme");
-		Review fifthMovie = new Review(400L, "Cube", "Movie description", "Review Category", "Content",
-				"/images/cube.jpg",
+		Review firstMovie = new Review(902L, "Identity", "Great thiller. Excellent ending.", "Thriller",
+				"Great plot. Excellent twists.", "/images/identity.jpg",
+				"A group of strangers are stranded in an isolated town, as they slowly die off...", "James Mangold");
+		Review secondMovie = new Review(121L, "John Wick", "Great action movie.", "Action", "Basic plot. Great action.",
+				"/images/john-wick.jpg", "After the brutal murder of Wick's dog, Wick goes on a hunt to get revenge.",
+				"Chad Stahelski, David Leitch");
+		Review thirdMovie = new Review(432L, "Texas Chainsaw Massacre", "Strange movie...worth a watch, but not more.",
+				"Horror", "Decent plot. Some gore. Odd ending that dragged on too long.",
+				"/images/texas-chainsaw-massacre.jpg",
+				"A group of friends is assaulted by a chainsaw-wielding murderer", "Tobe Hooper");
+		Review fourthMovie = new Review(223L, "Silence of the Lambs", "Mystery thriller with great acting. A+ movie.",
+				"Thriller", "Lots of thrills. Great acting by Hopkins and supporting cast.",
+				"/images/silence-of-the-lambs.jpg",
+				"Top FBI prospect Clarice Starling attempts to track down the deranged Buffalo Bill, but must enlist the help of the psycopathic Dr. Hannibal Lecter.",
+				"Jonathan Demme");
+		Review fifthMovie = new Review(400L, "Cube", "Interesting concept. Moderate acting.", "Thriller",
+				"Lots of tension. Relational drama throughout.", "/images/cube.jpg",
 				"A group of prisoners find themselves trapped in a large, "
 						+ "menacing cube and must work together to escape. Will they ever solve the cube?",
 				"Vincenzo Natali");
